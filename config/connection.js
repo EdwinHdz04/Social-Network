4e12b7f5675a0e8ced93d2fece4cb503783ec557
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-02-27 20:24:19 -0800

    updated config file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
-const {connect, connection} = require("mongoose");
+const mongoose = require("mongoose");
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
-connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-module.exports = connection;
+module.exports = mongoose.connection;
